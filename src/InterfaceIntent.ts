f5b2a169c0fb9f24ee19bcff59c95d52aef8a95f
ron-sepana <ron@teza.ai>
2024-04-01 13:02:43 +0300

    interface

--- src/InterfaceIntent.ts
+++ src/InterfaceIntent.ts
@@ -3,31 +3,63 @@ interface CurrencyFrom {
 	address: string
 	amount: string
 	chainId: number | string // Allow both number and string
-  }
-  
-  interface CurrencyTo {
+}
+
+interface CurrencyTo {
 	type: 'TOKEN'
 	address: string
 	amount: string
 	chainId: number | string // Allow both number and string
-  }
-  
-  interface StakeTo {
+}
+
+interface LiquidityProvidingTo {
+	type: 'LiquidityProviding',
+	asset_0: string, //ERC20 address
+	amount_0: number,
+	asset_1: string, //ERC20 address
+	amount_1: number,
+	address?: string //Explicit mention of a DeFi project. If empty - Solver will provide the best yield for this asset 
+}
+
+interface LiquidityWithdrawFrom {
+	type: 'LiquidityWithdraw',
+	tokenId: number | string // Allow both number and string,
+	liquidity: number,
+	amount0Min: number,
+	amount1Min: number,
+	deadline: Date
+	address: string 
+}
+
+interface LoanTo {
+	type: 'Loan',
+	asset: string, //ERC20 address
+	amount: number,
+	address?: string //Explicit mention of a DeFi project. If empty - Solver will provide the best yield for this asset 
+}
+
+interface LoanFrom {
+	type: 'Loan',
+	asset: string, //ERC20 address
+	amount: number,
+	address: string 
+}
+
+interface StakeTo {
 	type: 'STAKE'
 	address?: string
 	chainId?: number | string // Allow both number and string
-  }
-  
-  interface ExtraData {
+}
+
+interface ExtraData {
 	expirationDate?: Date
 	partiallyFillable?: boolean
-	kind: 'buy' | 'sell' | 'stake'
-  }
-  
-  export interface Intent {
+	kind: 'buy' | 'sell' | 'stake' | 'restake' | 'LP' | 'loan'   
+}
+
+export interface Intent {
 	sender: string
-	from: CurrencyFrom
-	to: CurrencyTo | StakeTo
+	from: CurrencyFrom | LoanFrom | LiquidityWithdrawFrom 
+	to: CurrencyTo | StakeTo | LoanTo | LiquidityProvidingTo
 	extraData?: ExtraData
-  }
-  
+}
