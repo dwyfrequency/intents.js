898d2f879671b73fed6490b9b3e269a91f7fe6f1
ron-sepana <ron@teza.ai>
2024-04-02 17:19:41 +0300

    fix types

--- src/InterfaceIntent.ts
+++ src/InterfaceIntent.ts
@@ -1,66 +1,32 @@
 interface CurrencyFrom {
 	type: 'TOKEN'
 	address: string
-	amount: number
+	amount: string
 	chainId: number | string // Allow both number and string
-}
-
-interface CurrencyTo {
+  }
+  
+  interface CurrencyTo {
 	type: 'TOKEN'
 	address: string
-	amount: number
+	amount: string
 	chainId: number | string // Allow both number and string
-}
-
-interface LiquidityProvidingTo {
-	type: 'LiquidityProviding',
-	asset_0: string, //ERC20 address
-	amount_0: number,
-	asset_1: string, //ERC20 address
-	amount_1: number,
-	address?: string //Explicit mention of a DeFi project. If empty - Solver will provide the best yield for this asset 
-}
-
-interface LiquidityWithdrawFrom {
-	type: 'LiquidityWithdraw',
-	tokenId: number | string // Allow both number and string,
-	liquidity: number,
-	amount0Min: number,
-	amount1Min: number,
-	deadline: Date
-	address: string 
-}
-
-interface LoanTo {
-	type: 'Loan',
-	asset: string, //ERC20 address
-	amount: number,
-	address?: string //Explicit mention of a DeFi project. If empty - Solver will provide the best yield for this asset 
-}
-
-interface LoanFrom {
-	type: 'Loan',
-	asset: string, //ERC20 address
-	amount: number,
-	address: string 
-}
-
-interface StakeTo {
+  }
+  
+  interface StakeTo {
 	type: 'STAKE'
-	address?: string,
-	amount: number,
+	address?: string
 	chainId?: number | string // Allow both number and string
-}
-
-interface ExtraData {
+  }
+  
+  interface ExtraData {
 	expirationDate?: Date
 	partiallyFillable?: boolean
-	kind: 'buy' | 'sell' | 'stake' | 'restake' | 'LP' | 'loan'   
-}
-
-export interface Intent {
+	kind: 'buy' | 'sell' | 'stake'
+  }
+  
+  export interface Intent {
 	sender: string
-	from: CurrencyFrom | LoanFrom | LiquidityWithdrawFrom 
-	to: CurrencyTo | StakeTo | LoanTo | LiquidityProvidingTo
+	from: CurrencyFrom
+	to: CurrencyTo | StakeTo
 	extraData?: ExtraData
-}
+  }
