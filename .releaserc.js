c80392a95f64b887d4b5138a4d62a4fa8e015efc
Lanre Adelowo <yo@lanre.wtf>
2024-09-18 11:24:33 +0100

    add .releaserc

---
+++ .releaserc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  branches: [
+    "main"
+  ], // Branch to trigger releases from
+  plugins: [
+    "@semantic-release/commit-analyzer", // Analyze commit messages
+    [
+      "@semantic-release/npm", // Publish to NPM
+      {
+        npmPublish: true,
+      },
+    ],
+    [
+      "@semantic-release/git", // Commit and push updated files
+      {
+        assets: [
+          "package.json"
+        ],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
