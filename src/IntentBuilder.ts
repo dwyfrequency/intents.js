99f117f2611077e2109c70337fb7b6f6ae00e203
EliasiOfir <ofir@eliasi.xyz>
2024-10-14 10:52:08 +0300

    fix: return type

--- src/IntentBuilder.ts
+++ src/IntentBuilder.ts
@@ -1,5 +1,5 @@
 import { BytesLike, ethers } from 'ethers';
-import { ChainConfig, isUserOpExecutionResponse, UserOpOptions } from './types';
+import { ChainConfig, isUserOpExecutionResponse, UserOpExecutionResponse, UserOpOptions } from './types';
 import {
   CALL_GAS_LIMIT,
   ENTRY_POINT,
@@ -13,7 +13,6 @@ import { FromState, State, ToState } from './index';
 import { Asset, Intent, Loan, Stake } from '.';
 import fetch from 'isomorphic-fetch';
 import { Account } from './Account';
-import { UserOperationEventEvent } from 'userop/dist/typechain/EntryPoint';
 
 /**
  * Facilitates the building and execution of Intent transactions.
@@ -54,12 +53,7 @@ export class IntentBuilder {
    * (important: though chainId is not required field which will be removed in future, we need it because our test network using custom chain IDs)
    * @returns A promise that resolves when the transaction has been executed.
    */
-  async execute(
-    from: State,
-    to: State,
-    account: Account,
-    chainId: number,
-  ): Promise<{ userOpHash: string; wait: () => Promise<UserOperationEventEvent | null> }> {
+  async execute(from: State, to: State, account: Account, chainId: number): Promise<UserOpExecutionResponse> {
     // TODO:: will be remove in future
     if (chainId === undefined || chainId === 0) {
       throw new Error('chainId is null or zero');
@@ -97,7 +91,7 @@ export class IntentBuilder {
     account: Account,
     chainId: number,
     opts: UserOpOptions,
-  ): Promise<{ userOpHash: string; wait: () => Promise<UserOperationEventEvent | null> }> {
+  ): Promise<UserOpExecutionResponse> {
     return await this._innerExecute(account, chainId, {
       calldata: opts.calldata ?? '0x',
       maxFeePerGas: opts.maxFeePerGas,
@@ -119,7 +113,7 @@ export class IntentBuilder {
       verificationGasLimit: string;
       callGasLimit: string;
     },
-  ): Promise<{ userOpHash: string; wait: () => Promise<UserOperationEventEvent | null> }> {
+  ): Promise<UserOpExecutionResponse> {
     const client = this._clients.get(chainId);
     if (!client) {
       throw new Error(`Client for chain ID ${chainId} not found`);
